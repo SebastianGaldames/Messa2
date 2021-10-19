c1eb7d76c2d914469b37beca33d30bd1741d8148
Escarlet <sakutwf@gmail.com>
2021-10-18 21:28:05 -0300

    Vista Login terminada

--- mesapage/src/views/About.vue
+++ mesapage/src/views/About.vue
@@ -1,3 +1,4 @@
+<template>
 <div>
 
     <h1>Sobre el proyecto</h1>
--- mesapage/src/views/Login.vue
+++ mesapage/src/views/Login.vue
@@ -5,42 +5,54 @@
   
       <b-col class="contenedor">
         <b-row class= "topRow">
-          <div class= "float-left">
-            <b-button href="/" variant= "basic" class="btn-img" size="lg"></b-button>
+          <div class= "float-left"> 
+            <b-card-img src="https://i.ibb.co/Rg9yp6V/Logo-MEssa.png" alt="Image" class="rounded-0 logo"></b-card-img>
           </div>
         </b-row>
-        <b-row>
-          <div class="barra">
-            <p>
-              Barrita iniciar sesion
-            </p>
-          </div>
+        <b-row class="barra" align-h="center" align-v="center">
+            <b-col cols="5">
+              <b-row>
+                <div class="icondiv">
+                  <b-card-img src="https://i.ibb.co/18JfHvf/585e4beacb11b227491c3399-4.png" alt="Image" class="icon"></b-card-img>
+                </div>
+              </b-row>
+            </b-col>
+            <b-col cols="7">
+              <b-row>
+                <div class="textdiv"><h3>
+                  Iniciar Sesión
+                  </h3>
+                </div>
+              </b-row>
+            </b-col>
         </b-row>
         <b-row class="centerRow">
-          <p>
+          <div>
             Usuario:
-          </p>
+          </div>
         </b-row>
         <b-row>
           <b-form-input
-            placeholder="Ingresa tu nombre de usuario" class="centerRow"
+            placeholder="Ingrese su nombre de usuario" class="centerRow userInput"
           ></b-form-input>
         </b-row>
-        <b-row class="centerRow topRow">
-          <p>
+        <b-row class="centerRow passInput">
+          <div>
             Contraseña:
-          </p>
+          </div>
         </b-row>
         <b-row>
             <b-form-input
               type="password" id="text-password" aria-describedby="password-help-block"
+              class="userInput" placeholder="Ingrese su contraseña"
             ></b-form-input>
         </b-row>
         <b-row>
-            <div>
+          <div class="buttonDiv">
             <b-button pill class="botonIniciarSesion topRow">Iniciar sesión</b-button>
           </div>
         </b-row>
+        <div class="buttonDiv">
         <b-row>
           <b-col>
             ¿No tienes cuenta?
@@ -51,6 +63,7 @@
           </b-col>
           
         </b-row>
+        </div>
       </b-col>
   </div>
 
@@ -70,13 +83,26 @@
   background-color: #1c335f;
 }
 
+.buttonDiv{
+  margin: 25px 0px;
+  margin-top: -10px;
+  margin-bottom: 10%;
+}
+.userInput{
+  margin-top: 10px;
+}
+
+.passInput{
+  margin-top: 5%;
+}
+
 .btn-img{
     background-image: url(https://i.ibb.co/Rg9yp6V/Logo-MEssa.png);
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    height: 40px;
-    width: 150px;
+    height: 15px;
+    width: auto;
 
   }
   
@@ -84,15 +110,16 @@
     width: 30%;
     margin-left: auto;
     margin-right: auto;
-    margin-top: 10%;
+    margin-top: 20px;
     border: 2px solid #1c335f;
     
   }
 
   .topRow{
-    margin-top: 5%;
+    margin-top: 10%;
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 10%;
   }
 
   .centerRow{
@@ -102,8 +129,42 @@
 
   .barra{
     background-color: #91B5F7;
-    margin: auto;
-    padding: 0px;
-    
+    padding-top: 10px;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
+    margin-top: 40px;
+  }
+
+  .icon{
+    height: 30px;
+    width: auto;
+  }
+
+  .icondiv{
+    text-align: end;
+    width: max;
+  }
+
+  .textdiv{
+    justify-self: start;
+    width: max;
+    text-align: start;
+    padding-left: 5px;
+    margin-top: 8px;
+  }
+
+  .userInput{
+    margin-left: auto;
+    margin-right: auto;
+    width: 70%;
+    font-size: 12px;
+    text-align: center;
+    border-radius: 10px;
+    border-color: transparent transparent #1c335f transparent;
+  }
+
+  .logo{
+    height: 130%;
+    width: auto;
   }
 </style>
