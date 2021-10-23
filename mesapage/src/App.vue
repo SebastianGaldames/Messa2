6348ceafb771eaa1dcd0846f4731c779e4de8f6d
Diego Aguilera <diegoaguilera4@icloud.com>
2021-10-23 17:15:44 -0300

    Creación interfaces de pago estáticas
    Se crearon las ventanas de pago y pago aceptado

--- mesapage/src/App.vue
+++ mesapage/src/App.vue
@@ -16,7 +16,8 @@
           <router-link to="/carrito">Carrito</router-link> |
           <router-link to="/blog">Danos tu Opinión</router-link> |
           <router-link to="/admin">Admin</router-link> |
-          <router-link to="/login">Iniciar Sesión</router-link>
+          <router-link to="/login">Iniciar Sesión</router-link> |
+          <router-link to="/pago">Pago</router-link>
         </div>
         </b-col>
       </b-row>
